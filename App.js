f5e47af6ad20358200f8e0f341e674e7dc01d7ca
Yanina Rekut <ykut2003@mail.ru>
2021-11-04 22:06:58 +0200

    no message

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Screen1 from './src/ScreensAll/Screen1';
+import Screen2 from './src/ScreensAll/Screen2.js';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Stack = createStackNavigator();
+
+      return (
+        <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Screen#1" component={Screen1} />
+          <Stack.Screen name="Screen#2" component={Screen2} />
+        </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+    
+  
+
 
 const styles = StyleSheet.create({
   container: {
